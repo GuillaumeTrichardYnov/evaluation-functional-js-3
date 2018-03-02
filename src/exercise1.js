d15980c7cbb72702784875dbd5ab0a5e3f564b86
Blandine Mallat <blandinemallat@MacBook-Pro-de-Blandine.local>
2018-03-02 09:44:06 +0100

    Exercise 1

--- src/exercise1.js
+++ src/exercise1.js
@@ -2,6 +2,26 @@ const moment = require("moment");
 
 const sortProduct = input => {
   // Your future job begins here ...
+  return input.reduce((acc, current) => {
+    var obj = {
+      monthOfYear : moment(current.dateAdded).month()+1,
+      name : current.name,
+      year : moment(current.dateAdded).year(),
+      quantity : current.quantity
+    }
+    if(acc){
+      if(obj.year > 2000){
+        if(acc.hasOwnProperty(current.name)){
+            acc[current.name].push(obj);
+            acc[current.name].sort(function(a, b){return a.monthOfYear - b.monthOfYear})
+            acc[current.name].sort(function(a, b){return a.year - b.year})
+        }else{
+            acc[current.name] = [obj];
+        }
+      }
+    }
+    return acc;
+  }, {})
 };
 
 module.exports = {

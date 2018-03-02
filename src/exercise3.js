317263a58ce36afb633c80e393ae01b05fab6093
Blandine Mallat <blandinemallat@MacBook-Pro-de-Blandine.local>
2018-03-02 10:52:36 +0100

    Exercise 3

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,9 +1,30 @@
 const _ = require("lodash");
 
 const meeteek = input => {
+
+function intersect(a, b) {
+    var t;
+    if (b.length > a.length) t = b, b = a, a = t; // indexOf to loop over shorter
+    return a.filter(function (e) {
+        return b.indexOf(e) > -1;
+    });
+}
   // Your future job begins here ...
-  input.reduce((acc, current) => {
+  return input.reduce((acc, current, index, array) => {
     acc[current.firstname] = [];
+    array.forEach(x => {
+      if(x.firstname != current.firstname)
+      {
+        if(x.sexPartnerApproved.includes(current.sex) && current.sexPartnerApproved.includes(x.sex))
+        {
+          if(intersect(x.interests, current.interests)){
+            acc[current.firstname].push(x);
+          }
+        }
+      }
+
+    })
+    return acc;
   }, {})
 };
 

27c1049688f6bada3a1d8fde7abe3a16bb55273f
Blandine Mallat <blandinemallat@MacBook-Pro-de-Blandine.local>
2018-03-02 10:30:31 +0100

    Exercise 4

--- src/exercise3.js
+++ src/exercise3.js
@@ -2,6 +2,9 @@ const _ = require("lodash");
 
 const meeteek = input => {
   // Your future job begins here ...
+  input.reduce((acc, current) => {
+    acc[current.firstname] = [];
+  }, {})
 };
 
 module.exports = {
--- src/exercise4.js
+++ src/exercise4.js
@@ -1,5 +1,16 @@
 const countSequence = (sequence, input) => {
   // Your future job begins here ...
+  if(input == "" || sequence == "")
+  {
+    return 0;
+  }
+  if(!input){
+      return function(input){
+          return (input.match(/11/g)|| []).length;
+      };
+  }else {
+      return (input.match(/11/g)|| []).length;
+  }
 };
 
 module.exports = {
